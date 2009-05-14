454cb4824b84150946368be926ff2a0f29527b0d
blmarket <blmarket@dbb752b6-32d3-11de-9d05-31133e6853b1>
2009-05-14 09:09:02 +0000

    git-svn-id: https://lib4bpp.googlecode.com/svn/trunk@16 dbb752b6-32d3-11de-9d05-31133e6853b1

--- 3DStudy/DxWinForm/Form1.cs
+++ 3DStudy/DxWinForm/Form1.cs
@@ -42,16 +42,55 @@ namespace DxWinForm
             vb.SetData((object)vertices,0,LockFlags.None);
         }
 
+        public void OnCreateBillBoard(object sender, EventArgs e)
+        {
+            CustomVertex.TransformedTextured[] points = new CustomVertex.TransformedTextured[4]
+            {
+                new CustomVertex.TransformedTextured(5,5,0,1,0,0),
+                new CustomVertex.TransformedTextured(5,105,0,1,0,1),
+                new CustomVertex.TransformedTextured(105,5,0,1,1,0),
+                new CustomVertex.TransformedTextured(105,105,0,1,1,1),
+            };
+
+            CustomVertex.TransformedTextured[] vertices = new CustomVertex.TransformedTextured[6]
+            {
+                points[0],
+                points[1],
+                points[3],
+                points[0],
+                points[2],
+                points[3],
+            };
+
+            VertexBuffer vb = (VertexBuffer)sender;
+            vb.SetData((object)vertices, 0, LockFlags.None);
+        }
+
         private void InitObjects()
         {
-            m_VB = new VertexBuffer(typeof(CustomVertex.PositionTextured), 6, dx_device, Usage.WriteOnly, CustomVertex.PositionTextured.Format,
+            VertexBuffer VB = new VertexBuffer(typeof(CustomVertex.PositionTextured), 6, dx_device, Usage.WriteOnly, CustomVertex.PositionTextured.Format,
                 Pool.Default);
-            m_VB.Created += new EventHandler(this.OnCreateVertexBuffer);
-            OnCreateVertexBuffer(m_VB, null);
+            VB.Created += new EventHandler(this.OnCreateVertexBuffer);
+            OnCreateVertexBuffer(VB, null);
+            m_Objects.Add(new KeyValuePair<int, VertexBuffer>(2, VB));
+
+            VB = new VertexBuffer(
+                typeof(CustomVertex.TransformedTextured), 
+                6, 
+                dx_device, 
+                Usage.WriteOnly, 
+                CustomVertex.TransformedTextured.Format, 
+                Pool.Default);
+
+            VB.Created += new EventHandler(this.OnCreateBillBoard);
+            OnCreateBillBoard(VB, null);
+            m_Objects.Add(new KeyValuePair<int, VertexBuffer>(2, VB));                
+
             m_Mesh = Mesh.Teapot(dx_device);
             Bitmap Bits = new Bitmap(512,512);
             Graphics g = Graphics.FromImage(Bits);
             Pen pen = new Pen(Color.Magenta);
+            pen.Width = 5.0f;
             g.Clear(Color.White);
             g.DrawRectangle(pen, 50, 50, 100, 100);
             Bits.Save("test.bmp");
@@ -66,6 +105,8 @@ namespace DxWinForm
 
             pp.Windowed = true;
             pp.SwapEffect = SwapEffect.Discard;
+            pp.EnableAutoDepthStencil = true;
+            pp.AutoDepthStencilFormat = DepthFormat.D16;
             try
             {
                 dx_device = new Device(0, DeviceType.Hardware, this, CreateFlags.SoftwareVertexProcessing, pp);
@@ -73,6 +114,9 @@ namespace DxWinForm
                 dx_device.RenderState.ColorVertex = true;
                 dx_device.RenderState.Lighting = false;
                 dx_device.RenderState.FillMode = FillMode.Solid;
+/*                dx_device.RenderState.StencilEnable = true;
+                dx_device.RenderState.ReferenceStencil = 0;
+                dx_device.RenderState.StencilPass = StencilOperation.Increment;*/
 
                 InitObjects();
             }
@@ -86,12 +130,10 @@ namespace DxWinForm
 
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
-            dx_device.Clear(ClearFlags.Target, Color.Black.ToArgb(), 1.0f, 0);
+            dx_device.Clear(ClearFlags.Target, Color.Black, 1.0f, 0);
 
             dx_device.BeginScene();
             dx_device.SetTexture(0, m_Tex);
-            dx_device.SetStreamSource(0, m_VB, 0);
-            dx_device.VertexFormat = m_VB.Description.VertexFormat;
 
             Matrix matWorld, matView, matProj;
             matWorld = matView = matProj = Matrix.Identity;
@@ -101,21 +143,30 @@ namespace DxWinForm
             dx_device.SetTransform(TransformType.View, matView);
             dx_device.SetTransform(TransformType.Projection, matProj);
 
-            dx_device.DrawPrimitives(PrimitiveType.TriangleList, 0, 2);
+            foreach (KeyValuePair<int, VertexBuffer> p in m_Objects)
+            {
+                dx_device.SetStreamSource(0, p.Value, 0);
+                dx_device.VertexFormat = p.Value.Description.VertexFormat;
+                dx_device.DrawPrimitives(PrimitiveType.TriangleList, 0, p.Key);
+            }
+
             dx_device.EndScene();
 
             dx_device.Present();
         }
 
         private Device dx_device = null;
-        private VertexBuffer m_VB = null;
+        private List<KeyValuePair<int, VertexBuffer>> m_Objects = new List<KeyValuePair<int, VertexBuffer>>();
         private Mesh m_Mesh = null;
         private Texture m_Tex = null;
 
         private void Form1_FormClosed(object sender, FormClosedEventArgs e)
         {
             if(m_Tex != null) m_Tex.Dispose();
-            m_VB.Dispose();
+            foreach (KeyValuePair<int, VertexBuffer> p in m_Objects)
+            {
+                p.Value.Dispose();
+            }
             m_Mesh.Dispose();           
             dx_device.Dispose();
         }
