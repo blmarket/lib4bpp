6ff57c01d7b8037aa77c518e42d36612168be6d1
blmarket <blmarket@dbb752b6-32d3-11de-9d05-31133e6853b1>
2009-12-10 05:41:39 +0000

    git-svn-id: https://lib4bpp.googlecode.com/svn/trunk@102 dbb752b6-32d3-11de-9d05-31133e6853b1

--- Finance/main.pl
+++ Finance/main.pl
@@ -4,28 +4,67 @@ use Finance::Quote;
 use Data::Dumper;
 
 $q = Finance::Quote->new;
-
 $q->timeout(60);
-
 #$conversion_rate = $q->currency("AUD","USD");
 $q->set_currency("KRW");  # Return all info in Euros.
-
 #$q->require_labels(qw/price date high low volume/);
-$q->require_labels(qw/price/);
-
+$q->require_labels(qw/price name/);
 $q->failover(1); # Set failover support (on by default).
 
-%stocks = { "069500.KS" => 230, "088980.KS" => 1100, "067250.KS" => 10, "000770.KS" => 10 };
+@labels = qw/price name/;
 
-@array = keys %stocks;
-print Dumper(@array);
+%stocks = ("069500.KS" => 230, "088980.KS" => 1200, "067250.KS" => 120, "000700.KS" => 50);
+$cash = "531546";
 
+%quotes  = $q->fetch("asia",keys %stocks);
 
-%quotes  = $q->fetch("asia","069500.KS", "088980.KS");
+my %result;
+foreach (keys %stocks)
+{
+	$result{$_}{"hold"} = $stocks{$_};
+}
 
 foreach (keys %quotes)
 {
-	print "$_ => $quotes{$_}\n" if /price$/;
+	$id = $n = undef;
+	foreach $candi (keys %stocks)
+	{
+		$id = $candi if /^$candi/;
+	}
+
+	foreach $name (@labels)
+	{
+		$n = $name if /$name$/
+	}
+
+	$result{$id}{$n} = $quotes{$_} if $id and $n;
 }
 
-print Dumper(%quotes);
+format STDOUT_TOP =
+name                 price       holds      total
+--------------------------------------------------------
+.
+
+format STDOUT =
+@<<<<<<<<<<<<<<<<<<  @<<<<<<<    @<<<<<<<<  @<<<<<<<<<<<<<
+$r{"name"} $r{"price"} $r{"hold"} $r{"price"}*$r{"hold"}
+.
+
+my $sum=0;
+
+foreach (keys %result)
+{
+	%r = %{$result{$_}};
+	$sum += $r{"price"} * $r{"hold"};
+	write STDOUT;
+#	print $result{$_}{"name"}."\t\t".$result{$_}{"price"}."\t\t".$result{$_}{"hold"}."\n";
+}
+
+print "--------------------------------------------------------\n";
+
+$sum += $cash;
+
+print "Cash : $cash\n";
+print "Total : $sum\n";
+
+#print Dumper(%result);
