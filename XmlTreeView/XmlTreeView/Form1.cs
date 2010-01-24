8a0230815ec843b4004a13f267fea15601e1f740
blmarket <blmarket@dbb752b6-32d3-11de-9d05-31133e6853b1>
2010-01-24 02:11:17 +0000

    Added Dynamic Update Feature
    git-svn-id: https://lib4bpp.googlecode.com/svn/trunk@128 dbb752b6-32d3-11de-9d05-31133e6853b1

--- XmlTreeView/XmlTreeView/Form1.cs
+++ XmlTreeView/XmlTreeView/Form1.cs
@@ -17,27 +17,6 @@ namespace XmlTreeView
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
-        {
-            try
-            {
-                XmlDocument dom = new XmlDocument();
-                dom.Load("Sample.XML");
-
-                treeView1.Nodes.Clear();
-                treeView1.Nodes.Add(new TreeNode(dom.DocumentElement.Name));
-                TreeNode tNode = new TreeNode();
-                tNode = treeView1.Nodes[0];
-
-                AddNode(dom.DocumentElement, tNode);
-                treeView1.ExpandAll();
-            }
-            catch (Exception E)
-            {
-                MessageBox.Show(E.Message);
-            }
-        }
-
         private void AddNode(XmlNode inXmlNode, TreeNode inTreeNode)
         {
             XmlNode xNode;
@@ -66,6 +45,81 @@ namespace XmlTreeView
             }
         }
 
+        private bool UpdateNode(XmlNode elem, TreeNodeCollection col)
+        {
+            bool changed = false;
+            if(col.Count > elem.ChildNodes.Count)
+            {
+                col.Clear();
+            }
+
+            for(int i=0;i<elem.ChildNodes.Count;i++)
+            {
+                XmlNode cnode = elem.ChildNodes[i];
+                string text;
+                if (cnode.HasChildNodes)
+                {
+                    text = cnode.Name;
+                }
+                else
+                {
+                    text = cnode.OuterXml.Trim();
+                }
+
+                if(col.Count <= i)
+                {
+                    col.Add(text);
+                    col[i].BackColor = Color.Red;
+                    changed = true;
+                }
+                else
+                {
+                    if(col[i].Text != text)
+                    {
+                        col[i].Text = text;
+                        col[i].BackColor = Color.Red;
+                        changed = true;
+                    }
+                }
+                if(UpdateNode(cnode, col[i].Nodes))
+                {
+                    col[i].BackColor = Color.Red;
+                    changed = true;
+                }
+            }
+            return changed;
+        }
+
+        private void LoadXML(string filename)
+        {
+            try
+            {
+                XmlDocument dom = new XmlDocument();
+                dom.Load(filename);
+
+                UpdateNode(dom.DocumentElement, treeView1.Nodes);
+/*
+
+                treeView1.Nodes.Clear();
+                treeView1.Nodes.Add(new TreeNode(dom.DocumentElement.Name));
+                TreeNode tNode = new TreeNode();
+                tNode = treeView1.Nodes[0];
+
+                AddNode(dom.DocumentElement, tNode);
+ */
+//                treeView1.ExpandAll();
+            }
+            catch (Exception E)
+            {
+                MessageBox.Show(E.Message);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            LoadXML("Sample.XML");
+        }
+
         Color[] avails = new Color[] {
             Color.Aqua, Color.Red, Color.Beige, Color.Bisque, Color.Black, Color.BlanchedAlmond, Color.Blue, Color.BlueViolet, Color.BurlyWood
         };
@@ -74,15 +128,38 @@ namespace XmlTreeView
         private void Form1_Load(object sender, EventArgs e)
         {
             Timer tt = new Timer();
-            tt.Interval = 2000;
+            tt.Interval = 50;
             tt.Tick += new EventHandler(tt_Tick);
             tt.Start();
         }
 
+        public int min(int a, int b)
+        {
+            if (a < b) return a;
+            return b;
+        }
+
         private void Traverse(TreeNode node)
         {
-            node.BackColor = avails[Picker.Next(avails.Length)];
+            Color curColor = node.BackColor;
+            if (curColor.ToArgb() == -1)
+            {
+                //               node.BackColor = Color.FromKnownColor((KnownColor)(Picker.Next(175)));
+            }
+            else
+            {
+                curColor = Color.FromArgb(min(255, curColor.R + 10), min(255, curColor.G + 10), min(255, curColor.B + 10));
+                node.BackColor = curColor;
+            }
             foreach (TreeNode cnode in node.Nodes)
+            {
+                Traverse(cnode);
+            }
+        }
+
+        private void Traverse(TreeNodeCollection col)
+        {
+            foreach(TreeNode cnode in col)
             {
                 Traverse(cnode);
             }
@@ -90,7 +167,7 @@ namespace XmlTreeView
 
         void tt_Tick(object sender, EventArgs e)
         {
-            Traverse(treeView1.Nodes[0]);
+            Traverse(treeView1.Nodes);
         }
     }
 }
--- XmlTreeView/XmlTreeView/Properties/AssemblyInfo.cs
+++ XmlTreeView/XmlTreeView/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("XmlTreeView")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCompany("blmarket.net")]
 [assembly: AssemblyProduct("XmlTreeView")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2010")]
+[assembly: AssemblyCopyright("Copyright © blmarket.net 2010")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // 모든 값을 지정하거나 아래와 같이 '*'를 사용하여 빌드 번호 및 수정 버전이 자동으로
 // 지정되도록 할 수 있습니다.
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
