1ffa42275a5da4c8383565c0cb2eb9ae9d33d0a7
blmarket <blmarket@dbb752b6-32d3-11de-9d05-31133e6853b1>
2010-01-29 07:31:55 +0000

    Current Status
    git-svn-id: https://lib4bpp.googlecode.com/svn/trunk@143 dbb752b6-32d3-11de-9d05-31133e6853b1

--- Finance/main.pl
+++ Finance/main.pl
@@ -16,13 +16,13 @@ $q->failover(1); # Set failover support (on by default).
 %stocks = (
 "035250.KS" => 10, 
 "067250.KS" => 120, 
-"069500.KS" => 190, 
+"069500.KS" => 50, 
 "088980.KS" => 1400, 
 "091160.KS" => 50,
 #"096040.KQ" => 200,
 "117930.KS" => 31, 
 );
-$cash = "1711609";
+$cash = "4714179";
 
 %quotes  = $q->fetch("asia",keys %stocks);
 
