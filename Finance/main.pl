25c6e60d553bd9c9fa574e9a3b8966d4553ad63a
blmarket <blmarket@dbb752b6-32d3-11de-9d05-31133e6853b1>
2010-03-30 07:09:05 +0000

    added invested column and applied current status
    git-svn-id: https://lib4bpp.googlecode.com/svn/trunk@156 dbb752b6-32d3-11de-9d05-31133e6853b1

--- Finance/main.pl
+++ Finance/main.pl
@@ -18,9 +18,9 @@ $q->failover(1); # Set failover support (on by default).
 %stocks = (
 "035250.KS" => 10, 
 "067250.KS" => 120, 
-"069500.KS" => 150, 
+"069500.KS" => 200, 
 "088980.KS" => 1000, 
-"091160.KS" => 50,
+"091160.KS" => 110,
 "102960.KS" => 100,
 "000660.KS" => 0,
 "000700.KS" => 0,
@@ -29,9 +29,14 @@ $q->failover(1); # Set failover support (on by default).
 );
 
 %invested = (
-"102960.KS" => 1617240,
+"035250.KS" => 159000,
+"067250.KS" => 1624000,
+"069500.KS" => 4415130,
+"088980.KS" => 4863199,
+"091160.KS" => 1749550,
+"102960.KS" => 1617000,
 );
-$cash = "4693930";
+$cash = "2612080";
 
 %quotes  = $q->fetch("asia",keys %stocks);
 
@@ -39,6 +44,7 @@ my %result;
 foreach (keys %stocks)
 {
 	$result{$_}{"hold"} = $stocks{$_};
+        $result{$_}{"invested"} = $invested{$_};
 }
 
 foreach (keys %quotes)
@@ -59,26 +65,39 @@ foreach (keys %quotes)
 
 format STDOUT_TOP =
 name                       price             holds             total
-------------------------------------------------------------------------
+-------------------------------------------------------------------------------
 .
 
 format STDOUT =
-@<<<<<<<<<<<<<<<<<<  @>>>>>>>(@<<<<<<<)    @<<<<<<<<  @>>>>>>>>>>>>>
-$r{"name"} $r{"price"} $r{"net"} $r{"hold"} $r{"price"}*$r{"hold"}
+@<<<<<<<<<<<<<<<<<<  @>>>>>>>(@<<<<<<<)    @<<<<<<<<  @>>>>>>>>>>>>>(@>>>>>%)
+$r{"name"} $r{"price"} $r{"net"} $r{"hold"} $r{"price"}*$r{"hold"} $r{"invested"}?$r{"price"}*$r{"hold"}/$r{"invested"}*100-100:"---"
 .
 
 my $sum=0;
 
 foreach (keys %result)
 {
-	%r = %{$result{$_}};
+    %r = %{$result{$_}};
+    if($r{"hold"} > 0)
+    {
 	$sum += $r{"price"} * $r{"hold"};
 	write STDOUT;
-#	print $result{$_}{"name"}."\t\t".$result{$_}{"price"}."\t\t".$result{$_}{"hold"}."\n";
+    }
 }
 
 
-print "------------------------------------------------------------------------\n";
+print "-------------------------------------------------------------------------------\n";
+
+foreach (keys %result)
+{
+    %r = %{$result{$_}};
+    if($r{"hold"} == 0)
+    {
+	write STDOUT;
+    }
+}
+
+print "-------------------------------------------------------------------------------\n";
 
 $sum += $cash;
 
