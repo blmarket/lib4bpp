a76b698a57cfc11e2e69615775d3b2838d0f114d
blmarket <blmarket@dbb752b6-32d3-11de-9d05-31133e6853b1>
2010-11-07 13:53:35 +0000

    업로드 폼 제작중
    git-svn-id: https://lib4bpp.googlecode.com/svn/trunk@177 dbb752b6-32d3-11de-9d05-31133e6853b1

--- banklogs/upload.php
+++ banklogs/upload.php
@@ -12,13 +12,64 @@
 <body> 
 
 <?
-  print_r($_FILES);
   if( is_uploaded_file( $_FILES['uploaded_file']['tmp_name'] ) )
-  {
+  {  
+    print_r($_FILES);
+
+    $handle = @fopen($_FILES['uploaded_file']['tmp_name'], "r");
+
+    if( ! $handle)
+    {
+        print("Error Occured");
+        return;
+    }
+
+    while( ! feof($handle))
+    {
+        $buffer = mb_convert_encoding(fgets($handle, 8192), "utf-8", "euc-kr");
+
+        if(strpos($buffer, "메모") != FALSE)
+        {
+            print("Memo Found asdf<br/>");
+            $bmt = "<table>" . mb_convert_encoding(fread($handle, $_FILES['uploaded_file']['size']), "utf-8", "euc-kr");
+
+            $dom = new domDocument;
+            $dom->loadHTML($bmt);
+
+            print($dom->actualEncoding . "<br/>");
+
+            $dom->preserveWhiteSpace = false;
+
+            $tables = $dom->getElementsByTagName('table');
+
+            $table = $tables->item(0);
+
+            /*** get all rows from the table ***/
+            $rows = $table->getElementsByTagName('tr');
+
+            /*** loop over the table rows ***/
+            foreach ($rows as $row)
+            {
+                /*** get each column by tag name ***/
+                $cols = $row->getElementsByTagName('td');
+                /*** echo the values ***/
+                echo mb_convert_encoding($cols->item(0)->nodeValue, "euc-kr", "utf-8").'<br />';
+                echo $cols->item(1)->nodeValue.'<br />';
+                echo $cols->item(2)->nodeValue;
+                echo '<hr />'; 
+            }
+        }
+    }
+
+    fclose($handle);
+    
     $fileData = file_get_contents($_FILES['uploaded_file']['tmp_name']);
+
+    /*
     ?>
     $('div').html('<?=mb_convert_encoding($fileData, "utf-8", "euc-kr")?>').appendTo('form');
     <?
+    */
   }
   else
   {
